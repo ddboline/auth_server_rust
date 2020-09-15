e416c6ac7e9ff2bb5d81fb2332e58d6400c04e73
Daniel Boline <ddboline@gmail.com>
2020-09-15 14:31:51 -0400

    constuct integration test which creates invitation, registers randomly generated user, logs in as user, changes password as user, verifies password change, then deletes user

--- src/app.rs
+++ src/app.rs
@@ -5,11 +5,12 @@ use lazy_static::lazy_static;
 use rand::{thread_rng, Rng};
 use std::time::Duration;
 use tokio::{task::spawn, time::interval};
+use stack_string::StackString;
 
 use crate::{
     config::Config,
     google_openid::GoogleClient,
-    logged_user::{create_secret, fill_auth_from_db, update_secret, JWT_SECRET, SECRET_KEY},
+    logged_user::{create_secret, fill_auth_from_db, update_secret, JWT_SECRET, SECRET_KEY, KEY_LENGTH},
     pgpool::PgPool,
     routes::{
         auth_url, callback, change_password_user, get_me, login, logout, register_email,
@@ -49,6 +50,10 @@ pub struct AppState {
 }
 
 pub async fn start_app() -> Result<(), Error> {
+    run_app(CONFIG.port, SECRET_KEY.load(), CONFIG.domain.clone()).await
+}
+
+async fn run_app(port: u32, cookie_secret: [u8; KEY_LENGTH], domain: StackString) -> Result<(), Error> {
     async fn _update_db(pool: PgPool) {
         let mut i = interval(Duration::from_secs(60));
         loop {
@@ -72,10 +77,10 @@ pub async fn start_app() -> Result<(), Error> {
             .data(google_client.clone())
             .data(AppState { pool: pool.clone() })
             .wrap(IdentityService::new(
-                CookieIdentityPolicy::new(&SECRET_KEY.load())
+                CookieIdentityPolicy::new(&cookie_secret)
                     .name("auth")
                     .path("/")
-                    .domain(CONFIG.domain.as_str())
+                    .domain(domain.as_str())
                     .max_age(CONFIG.expiration_seconds)
                     .secure(false),
             ))
@@ -112,7 +117,7 @@ pub async fn start_app() -> Result<(), Error> {
                     ),
             )
     })
-    .bind(&format!("127.0.0.1:{}", CONFIG.port))?
+    .bind(&format!("localhost:{}", port))?
     .run()
     .await
     .map_err(Into::into)
@@ -121,14 +126,84 @@ pub async fn start_app() -> Result<(), Error> {
 #[cfg(test)]
 mod tests {
     use anyhow::Error;
+    use maplit::hashmap;
 
-    use crate::app::get_random_string;
+    use crate::logged_user::{get_random_key, KEY_LENGTH, LoggedUser};
+    use crate::app::{get_random_string, run_app, CONFIG};
+    use crate::pgpool::PgPool;
+    use crate::invitation::Invitation;
+    use crate::user::User;
 
     #[test]
     fn test_get_random_string() -> Result<(), Error> {
         let rs = get_random_string(32);
         println!("{}", rs);
         assert_eq!(rs.len(), 32);
+        Ok(())
+    }
+
+    #[actix_rt::test]
+    #[ignore]
+    async fn test_create_user() -> Result<(), Error> {
+        let pool = PgPool::new(&CONFIG.database_url);
+        let email = format!("{}@localhost", get_random_string(32));
+        let password = get_random_string(32);
+        let invitation = Invitation::from_email(&email);
+        let uuid = invitation.id.clone().to_string();
+        invitation.insert(&pool).await?;
+
+        let mut secret_key = [0u8; KEY_LENGTH];
+        secret_key.copy_from_slice(&get_random_key());
+        let test_port = 12345;
+        actix_rt::spawn(async move {run_app(test_port, secret_key, "localhost".into()).await.unwrap()});
+
+        tokio::time::delay_for(tokio::time::Duration::from_secs(10)).await;
+
+        let url = format!("http://localhost:{}/api/register/{}", test_port, uuid);
+        let data = hashmap! {
+            "password" => &password,
+        };
+
+        let client = reqwest::Client::builder().cookie_store(true).build()?;
+        println!("register");
+        let resp: LoggedUser = client.post(&url).json(&data).send().await?.error_for_status()?.json().await?;
+        println!("registered {:?}", resp);
+        assert_eq!(resp.email.as_str(), email.as_str());
+
+        assert!(Invitation::get_by_uuid(&uuid, &pool).await?.is_none());
+
+        let url = format!("http://localhost:{}/api/auth", test_port);
+        let data = hashmap! {
+            "email" => &email,
+            "password" => &password,
+        };
+        println!("login");
+        let resp: LoggedUser = client.post(&url).json(&data).send().await?.error_for_status()?.json().await?;
+        println!("logged in {:?}", resp);
+        assert_eq!(resp.email.as_str(), email.as_str());
+
+        println!("get me");
+        let resp: LoggedUser = client.get(&url).send().await?.error_for_status()?.json().await?;
+        println!("I am: {:?}", resp);
+        assert_eq!(resp.email.as_str(), email.as_str());
+
+        let url = format!("http://localhost:{}/api/password_change", test_port);
+        let new_password = get_random_string(32);
+        let data = hashmap! {
+            "email" => &email,
+            "password" => &new_password,
+        };
+        println!("change password");
+        let text = client.post(&url).json(&data).send().await?.error_for_status()?.text().await?;
+        println!("password changed {:?}", text);
+        assert_eq!(text.as_str(), "password updated");
+
+        let user = User::get_by_email(&email, &pool).await?.unwrap();
+        assert!(user.verify_password(&new_password)?);
+
+        user.delete(&pool).await?;
+        assert_eq!(User::get_by_email(&email, &pool).await?, None);
+
         Ok(())
     }
 }
