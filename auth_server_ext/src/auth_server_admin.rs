2e5c5a4ba3462b07d4c3f1569864a91b01c077f6
Daniel Boline <ddboline@gmail.com>
2021-03-13 13:49:38 -0500

    add type

--- auth_server_ext/src/auth_server_admin.rs
+++ auth_server_ext/src/auth_server_admin.rs
@@ -90,7 +90,7 @@ enum AuthServerOptions {
 
 impl AuthServerOptions {
     #[allow(clippy::too_many_lines)]
-    pub async fn process_args(&self, pool: &PgPool, stdout: &StdoutChannel) -> Result<(), Error> {
+    pub async fn process_args(&self, pool: &PgPool, stdout: &StdoutChannel<StackString>) -> Result<(), Error> {
         let config = Config::init_config()?;
         match self {
             AuthServerOptions::List => {
