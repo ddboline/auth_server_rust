ab840998333f47799112e81194f6d6772239fe51
Daniel Boline <ddboline@gmail.com>
2021-10-07 21:09:11 -0400

    try from claimset, PrivateClaim

--- authorized_users/src/claim.rs
+++ authorized_users/src/claim.rs
@@ -1,8 +1,10 @@
-use biscuit::RegisteredClaims;
+use biscuit::{RegisteredClaims, ClaimsSet};
 use chrono::{Duration, Utc};
 use serde::{Deserialize, Serialize};
 use stack_string::StackString;
 use uuid::Uuid;
+use std::convert::TryFrom;
+use anyhow::{Error, format_err};
 
 use crate::AuthorizedUser;
 
@@ -14,17 +16,25 @@ pub struct Claim {
     issued_at: i64,
     email: StackString,
     session: Uuid,
+    secret_key: StackString,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct PrivateClaim {
+    email: StackString,
+    secret_key: StackString,
 }
 
 // struct to get converted to token and back
 impl Claim {
-    pub fn with_email(email: &str, domain: &str, expiration_seconds: i64, session: Uuid) -> Self {
+    pub fn with_email(email: &str, domain: &str, expiration_seconds: i64, session: Uuid, secret_key: &str) -> Self {
         Self {
             domain: domain.into(),
             email: email.into(),
             session,
             issued_at: Utc::now().timestamp(),
             expiry: (Utc::now() + Duration::seconds(expiration_seconds)).timestamp(),
+            secret_key: secret_key.into(),
         }
     }
 
@@ -46,6 +56,31 @@ impl Claim {
             ..RegisteredClaims::default()
         }
     }
+
+    pub fn get_private_claims(&self) -> PrivateClaim {
+        PrivateClaim {
+            email: self.email.clone(),
+            secret_key: self.secret_key.clone(),
+        }
+    }
+}
+
+impl TryFrom<ClaimsSet<PrivateClaim>> for Claim {
+    type Error = Error;
+
+    fn try_from(claim_set: ClaimsSet<PrivateClaim>) -> Result<Self, Self::Error> {
+        let reg = &claim_set.registered;
+        let pri = claim_set.private;
+        let session: Uuid = reg.id.as_ref().ok_or_else(|| format_err!("No session"))?.parse()?;
+        let domain = reg.issuer.as_ref().ok_or_else(|| format_err!("No domain"))?.into();
+        let expiry = reg.expiry.ok_or_else(|| format_err!("No expiry"))?.timestamp();
+        let issued_at = reg.issued_at.ok_or_else(|| format_err!("No iss"))?.timestamp();
+        let email = pri.email;
+        let secret_key = pri.secret_key;
+        Ok(Self {
+            domain, expiry, issued_at, email, session, secret_key,
+        })
+    }
 }
 
 impl From<Claim> for AuthorizedUser {
@@ -53,6 +88,7 @@ impl From<Claim> for AuthorizedUser {
         Self {
             email: claim.get_email().into(),
             session: claim.session,
+            secret_key: claim.secret_key,
         }
     }
 }
