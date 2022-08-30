7901b0f70819614c06a92f3bcc8c6dbd85a91bc8
Daniel Boline <ddboline@gmail.com>
2022-08-30 19:06:01 -0400

    dhat

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use auth_server_http::app::start_app;
 
+#[cfg(feature = "dhap-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+
 #[tokio::main]
 #[cfg(not(tarpaulin_include))]
 async fn main() {
+
+    #[cfg(feature = "dhat-heap")]
+    let _profiler = dhat::Profiler::new_heap();
+
     env_logger::init();
     start_app().await.unwrap();
 }
