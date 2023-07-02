4f2f5a93f8df94b75b7f98dd230489415c2e5719
Daniel Boline <ddboline@gmail.com>
2023-07-02 14:42:18 -0400

    forgot to add new errors

---
+++ authorized_users/src/errors.rs
@@ -0,0 +1,22 @@
+use biscuit::errors::Error as BiscuitError;
+use reqwest::{header::InvalidHeaderValue, Error as ReqwestError};
+use stack_string::StackString;
+use thiserror::Error;
+use url::ParseError as UrlParseError;
+use uuid::Error as UuidError;
+
+#[derive(Error, Debug)]
+pub enum AuthUsersError {
+    #[error("Token error {0}")]
+    TokenError(StackString),
+    #[error("Biscuit Error{0}")]
+    BiscuitError(#[from] BiscuitError),
+    #[error("UUID Error {0}")]
+    UuidError(#[from] UuidError),
+    #[error("InvalidHeaderValue {0}")]
+    InvalidHeaderValue(#[from] InvalidHeaderValue),
+    #[error("ReqwestError {0}")]
+    ReqwestError(#[from] ReqwestError),
+    #[error("UrlParseError {0}")]
+    UrlParseError(#[from] UrlParseError),
+}
