00a915d59e11c834adb0c56b37607c07bf64429c
Daniel Boline <ddboline@gmail.com>
2023-10-19 21:14:21 -0400

    login_user_jwt

--- auth_server_http/src/auth.rs
+++ auth_server_http/src/auth.rs
@@ -1,11 +1,16 @@
+use log::error;
 use rweb::Schema;
 use serde::Deserialize;
 use stack_string::StackString;
 use tokio::task::spawn_blocking;
 
-use auth_server_lib::{pgpool::PgPool, user::User};
+use auth_server_lib::{config::Config, pgpool::PgPool, session::Session, user::User};
+use authorized_users::AuthorizedUser;
 
-use crate::errors::ServiceError as Error;
+use crate::{
+    errors::ServiceError as Error,
+    logged_user::{LoggedUser, UserCookies},
+};
 
 #[derive(Debug, Deserialize, Schema)]
 pub struct AuthRequest {
@@ -36,6 +41,33 @@ impl AuthRequest {
             spawn_blocking(move || User::fake_verify(&password)).await??;
         }
         Err(Error::BadRequest("Invalid username or password"))
+    }
+
+    /// # Errors
+    /// Returns error if db query fails
+    pub async fn login_user_jwt(
+        &self,
+        pool: &PgPool,
+        config: &Config,
+    ) -> Result<(LoggedUser, Session, UserCookies<'static>), Error> {
+        let session = Session::new(self.email.as_str());
+        session.insert(pool).await.map_err(|e| {
+            error!("error on session insert {e}");
+            e
+        })?;
+
+        let user = self.authenticate(pool).await?;
+        let user: AuthorizedUser = user.into();
+        let mut user: LoggedUser = user.into();
+        user.session = session.id.into();
+        user.secret_key = session.secret_key.clone();
+        let cookies = user
+            .get_jwt_cookie(&config.domain, config.expiration_seconds, config.secure)
+            .map_err(|e| {
+                error!("Failed to create_token {e}");
+                Error::BadRequest("Failed to create_token")
+            })?;
+        Ok((user, session, cookies))
     }
 }
 
