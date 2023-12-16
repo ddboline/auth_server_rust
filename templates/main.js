ce78600907551f82d069f5f73455f2644735afcf
Daniel Boline <ddboline@gmail.com>
2023-12-15 22:59:43 -0500

    add functions

--- templates/main.js
+++ templates/main.js
@@ -12,3 +12,105 @@ function post(url = ``, data = {}) {
     body: JSON.stringify(data), // body data type must match "Content-Type" header
   }).then(response => response.json()); // parses response to JSON
 }
+function logout() {
+  let url = "/api/auth";
+  let xmlhttp = new XMLHttpRequest();
+  xmlhttp.onload = function() {
+      location.reload()
+  }
+  xmlhttp.open("DELETE", url, true);
+  xmlhttp.send(null);
+}
+function delete_session_data( session_key ) {
+  let url = "/api/sessions?session_key=" + session_key;
+  let xmlhttp = new XMLHttpRequest();
+  xmlhttp.onload = function() {
+      list_sessions();
+      list_session_data();
+  }
+  xmlhttp.open("DELETE", url, true);
+  xmlhttp.send(null);
+}
+function delete_session( session_id ) {
+  let url = "/api/sessions?session=" + session_id;
+  let xmlhttp = new XMLHttpRequest();
+  xmlhttp.onload = function() {
+      list_sessions();
+      list_session_data();
+  }
+  xmlhttp.open("DELETE", url, true);
+  xmlhttp.send(null);
+}
+function registerViaEmail() {
+  let email = document.querySelector('#email');
+
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onload = function() {
+    email.value = '';
+    document.getElementsByClassName("login").innerHTML = "Please check your email.";
+  }
+  var data = JSON.stringify({"email": email.value});
+  xmlhttp.open( "POST", "/api/invitation", true );
+  xmlhttp.setRequestHeader("Content-Type", "application/json");
+  xmlhttp.send(data);
+}
+function openIdConnectLogin() {
+  let url = "/api/auth_url";
+  let final_url = getFinalUrl();
+  if(final_url == null ) {
+    final_url = window.location.href;
+  }
+  let data = JSON.stringify({"final_url": final_url});
+  let xmlhttp = new XMLHttpRequest();
+  xmlhttp.onload = function() {
+    js = JSON.parse(xmlhttp.responseText);
+
+    let xmlhttp2 = new XMLHttpRequest();
+    xmlhttp2.onload = function() {
+      location.replace(final_url);
+    }
+    let url2 = "/api/await?state=" + js.csrf_state;
+    xmlhttp2.open("GET", url2, true);
+    xmlhttp2.send(null);
+
+    window.open(js.auth_url, '_blank');
+    window.focus();
+  }
+  xmlhttp.open("POST", url, true);
+  xmlhttp.setRequestHeader("Content-Type", "application/json");
+  xmlhttp.send(data);
+}
+function getFinalUrl() {
+  let params = new URLSearchParams(window.location.search);
+  return params.get('final_url');
+}
+function login() {
+  let final_url = getFinalUrl();
+  let email = document.querySelector('#email');
+  let password = document.querySelector('#password');
+  var data = JSON.stringify({"email": email.value, "password": password.value});
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onload = function() {
+    if(final_url) {
+      location.replace(final_url);
+    } else {
+      location.reload();
+    }
+  }
+  xmlhttp.open( "POST", '/api/auth' , true );
+  xmlhttp.setRequestHeader("Content-Type", "application/json");
+  xmlhttp.send(data);
+}
+function sendVerificationEmail() {
+  let email = document.querySelector('#email');
+
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onload = function() {
+    email.value = '';
+    document.getElementsByClassName("login").innerHTML = "Please check your email.";
+  }
+  var data = JSON.stringify({"email": email.value});
+  xmlhttp.open( "POST", "/api/password_change", true );
+  xmlhttp.setRequestHeader("Content-Type", "application/json");
+  xmlhttp.send(data);
+}
